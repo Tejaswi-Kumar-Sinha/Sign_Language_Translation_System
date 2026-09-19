4df9336af57914c24ebb7e74515fbcbaaedb541a
Siddharth Kaushik <siddharthkaushik2007@gmail.com>
2026-09-19 20:35:15 +0530

    update3

--- slts.py
+++ slts.py
@@ -17,7 +17,14 @@ def mediapipe_detection(image, model):
     image.flags.writeable = True#Image is now writeable
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)#Color conversion RGB 2 BGR
     return image,results
- 
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) #Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) #Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw right hand connections
+
+    mp_holistic.POSE_CONNECTIONS
 #using opencv
 cap = cv2.VideoCapture(0)#accessing webcam
 #set mediapipe model
@@ -29,9 +36,12 @@ with mp_holistic.Holistic(min_detection_confidence=0.5 , min_tracking_confidence
     #Make detections
     image,results=mediapipe_detection(frame,holistic)
     print(results)
-    
+
+    #Draw landmarks
+    draw_landmarks(image,results)
+
     #show to screen
-    cv2.imshow('OpenCV Feed', frame)#Can change OpenCV Feed:Just a name of interface
+    cv2.imshow('OpenCV Feed', image)#Can change OpenCV Feed:Just a name of interface
 
     #break gracefully
     if cv2.waitKey(10) & 0xFF == ord('q'):#press q to quit
