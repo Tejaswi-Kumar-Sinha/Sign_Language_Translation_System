18961cc660c8218ead4a50361234f9cf3b26f410
Tejaswi Kumar Sinha <tejaswisinha117@gmail.com>
2026-09-19 21:36:56 +0530

    update-4

--- slts.py
+++ slts.py
@@ -17,14 +17,31 @@ def mediapipe_detection(image, model):
     image.flags.writeable = True#Image is now writeable
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)#Color conversion RGB 2 BGR
     return image,results
-
+#media pipe draw function
 def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) #Draw face connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) #Draw pose connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw left hand connections
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) #Draw right hand connections
-
-    mp_holistic.POSE_CONNECTIONS
+#using media pipe and stylising the drawings
+def draw_styled_landmarks(image,results):
+   mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                            mp_drawing.DrawingSpec(color=(80,110,10),thickness=1, circle_radius=1),
+                            mp_drawing.DrawingSpec(color=(80,256,121),thickness=1, circle_radius=1))
+    #Draw face connections
+   mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10),thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(80,44,121),thickness=2, circle_radius=2))
+    #Draw pose connections
+   mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121,22,76),thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(121,44,250),thickness=2, circle_radius=2))
+    #Draw left hand connections
+   mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245,117,66),thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245,66,230),thickness=2, circle_radius=2))
+    #Draw right hand connections
+   
 #using opencv
 cap = cv2.VideoCapture(0)#accessing webcam
 #set mediapipe model
@@ -38,7 +55,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5 , min_tracking_confidence
     print(results)
 
     #Draw landmarks
-    draw_landmarks(image,results)
+    draw_styled_landmarks(image,results)
 
     #show to screen
     cv2.imshow('OpenCV Feed', image)#Can change OpenCV Feed:Just a name of interface
@@ -47,4 +64,4 @@ with mp_holistic.Holistic(min_detection_confidence=0.5 , min_tracking_confidence
     if cv2.waitKey(10) & 0xFF == ord('q'):#press q to quit
         break
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
